5d30411ba5b10cc8639c8deb38f45e29341feb51
Elle Whaley <ellie.whaley@bynd.com>
2019-10-10 16:43:09 +0100

    got api working successfully and added some styling

---
+++ beyond-music/src/app/data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private ROOT_URL =
+    // tslint:disable-next-line: max-line-length
+    'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails,status&maxResults=10&playlistId=PLSi28iDfECJPJYFA4wjlF5KUucFvc0qbQ&key=AIzaSyCuv_16onZRx3qHDStC-FUp__A6si-fStw';
+
+  constructor(private http: HttpClient) {}
+
+  sendGetRequest() {
+    return this.http.get(this.ROOT_URL);
+  }
+}
--- beyond-music/src/app/home/home.component.ts
+++ beyond-music/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  videos = [];
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
+    this.dataService.sendGetRequest().subscribe((data: any) => {
+      console.log(data);
+      this.videos = data.items;
+    });
   }
-
 }
